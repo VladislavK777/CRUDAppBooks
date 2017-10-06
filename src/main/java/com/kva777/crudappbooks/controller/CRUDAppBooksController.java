1240f0d9272e65b347656436bbc94899329b0d5b
Vladislav.Klochkov <Vladislav.Klochkov@billing.ru>
2017-10-06 21:16:44 +0300

    Fix pack

--- src/main/java/com/kva777/crudappbooks/controller/CRUDAppBooksController.java
+++ src/main/java/com/kva777/crudappbooks/controller/CRUDAppBooksController.java
@@ -19,7 +19,6 @@ import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.portlet.ModelAndView;
 
 import java.util.List;
 
